95de92f7492ed1b08578ce63a3d0ed6181b26aee
CrossField1973 <45197889+CrossField1973@users.noreply.github.com>
2020-05-02 15:38:32 +0200

    added setup.js
    instanciates all modules

---
+++ JS/setup.js
@@ -0,0 +1,6 @@
+befindlichkeit = new Befindlichkeit;
+darstellung = new Darstellung ;
+import_export = new Import_Export ;
+konfiguration = new Konfiguration ;
+statistik = new Statistik ;
+steuerung = new Steuerung ;
